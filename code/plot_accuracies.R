6b9ed8a92f711ecb628e1687dc6d5bd99926b2c4
Ubuntu <prueba@testdai.cz5tryqij2mupcc1sabfj05qld.ex.internal.cloudapp.net>
2018-05-02 01:12:01 +0000

    Only experiments run so far with new code

--- code/plot_accuracies.R
+++ code/plot_accuracies.R
@@ -2,21 +2,11 @@ library(dplyr)
 library(tidyr)
 library(readr)
 
-accuracies_all <- read_csv("../out/MNIST/bald/MNIST_bald_accuracies_so_far.csv") %>% 
-  mutate(iter = 1:nrow(.)) %>% 
+accuracies_all <- read_csv("../out/MNIST/random_acq//MNIST_accuracies_so_far_random.csv") %>% 
   bind_rows(
-    read_csv("../out/MNIST/predictive_entropy/MNIST_predictive_entropy_accuracies_so_far.csv") %>% 
-      mutate(iter = 1:nrow(.))
+    read_csv("../out/MNIST/var_ratios/MNIST_accuracies_so_far_var_ratios.csv")
   ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/random_acq/MNIST_random_accuracies_so_far.csv") %>% 
-      mutate(iter = 1:nrow(.))
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios/MNIST_var_ratios_accuracies_so_far.csv") %>% 
-      mutate(iter = 1:nrow(.))
-  ) %>% 
-  mutate(num_images = 20 + (iter-1)*10)
+  mutate(num_images = 20 + iter*10)
 
 saveRDS(accuracies, "../out/MNIST/accuracies_all.rds")
   
