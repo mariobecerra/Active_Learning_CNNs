7c5a25cc1d190ed9bfdc63b57869f7e604066a80
Ubuntu <prueba@testdai.cz5tryqij2mupcc1sabfj05qld.ex.internal.cloudapp.net>
2018-05-04 19:10:19 +0000

    Add simple model

--- code/utils_MNIST.py
+++ code/utils_MNIST.py
@@ -6,6 +6,35 @@
 #   shuffled_indices = np.random.permutation(indices)
 #   return shuffled_indices
 
+def simple_MNIST_model():
+  img_rows, img_cols = 28, 28
+  if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_rows, img_cols)
+  else:
+    input_shape = (img_rows, img_cols, 1)
+  
+  num_classes = 10
+
+  # Specify architecture
+  model = Sequential()
+  model.add(Conv2D(32, kernel_size=(3, 3),
+                   activation='relu',
+                   input_shape=input_shape))
+  model.add(Conv2D(64, (3, 3), activation='relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Dropout(0.25))
+  model.add(Flatten())
+  model.add(Dense(128, activation='relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(num_classes, activation='softmax'))
+  
+  model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+  
+  print("Compiled model")
+  return(model)
+
 
 def MNIST_model(n_train):
   img_rows, img_cols = 28, 28
