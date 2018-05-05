0f640d3baf1926b22febe784079d1410bebbc8f3
mbc <mbecerracontreras@gmail.com>
2018-05-04 21:15:43 -0500

    Correct model, it should be the simple one and with size 4 convolution filters, also add L2 regularization

--- code/utils_MNIST.py
+++ code/utils_MNIST.py
@@ -6,37 +6,46 @@
 #   shuffled_indices = np.random.permutation(indices)
 #   return shuffled_indices
 
-def simple_MNIST_model():
+def MNIST_model(n_train):
   img_rows, img_cols = 28, 28
   if K.image_data_format() == 'channels_first':
     input_shape = (1, img_rows, img_cols)
   else:
     input_shape = (img_rows, img_cols, 1)
   
-  num_classes = 10
+  # number of convolutional filters to use
+  nb_filters = 32
+  # size of pooling area for max pooling
+  nb_pool = 2
+  # convolution kernel size
+  nb_conv = 4
+
+  nb_classes = 10
+
+  c = 2.5
+  Weight_Decay = c / float(n_train)
 
   # Specify architecture
   model = Sequential()
-  model.add(Conv2D(32, kernel_size=(3, 3),
+  model.add(Conv2D(nb_filters, kernel_size=(nb_conv, nb_conv),
                    activation='relu',
                    input_shape=input_shape))
-  model.add(Conv2D(64, (3, 3), activation='relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Conv2D(nb_filters, (nb_conv, nb_conv), activation='relu'))
+  model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
   model.add(Dropout(0.25))
   model.add(Flatten())
-  model.add(Dense(128, activation='relu'))
+  model.add(Dense(128, W_regularizer=l2(Weight_Decay)))
+  model.add(Activation('relu'))
   model.add(Dropout(0.5))
-  model.add(Dense(num_classes, activation='softmax'))
-  
-  model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy'])
+  model.add(Dense(nb_classes))
+  model.add(Activation('softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   
   print("Compiled model")
   return(model)
 
 
-def MNIST_model(n_train):
+def MNIST_model_deeper(n_train):
   img_rows, img_cols = 28, 28
   # number of convolutional filters to use
   nb_filters = 32
