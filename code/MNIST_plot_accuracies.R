35e0dbcfc50f17124eaf13687ec7a9a66c3da962
Mario <mbecerracontreras@gmail.com>
2018-12-02 12:00:44 -0600

    Fix plot script to average accuracies

--- code/MNIST_plot_accuracies.R
+++ code/MNIST_plot_accuracies.R
@@ -8,6 +8,7 @@ theme_set(theme_bw())
 
 out_dirs = list.dirs("../out/MNIST")
 
+
 random_dirs = grep("random", out_dirs, ignore.case = T, value = T)
 
 random_accuracies = map_df(seq_along(random_dirs), function(i){
@@ -16,167 +17,95 @@ random_accuracies = map_df(seq_along(random_dirs), function(i){
     mutate(run_id = i)
 })
 
-accuracies_all <- read_csv("../out/MNIST/random_acq/MNIST_accuracies_so_far_random.csv") %>% 
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios/MNIST_accuracies_so_far_var_ratios.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/bald/MNIST_accuracies_so_far_bald.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/predictive_entropy/MNIST_accuracies_so_far_predictive_entropy.csv")
-  ) %>%
-  bind_rows(
-    read_csv("../out/MNIST/freq_var_ratios/MNIST_accuracies_so_far_freq_var_ratios.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/freq_predictive_entropy/MNIST_accuracies_so_far_freq_predictive_entropy.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/predictive_entropy_2018-05-07_no_dropout_test_time_50_MC_samples/MNIST_accuracies_so_far_predictive_entropy.csv") %>% 
-      mutate(acq_fun = "pred_ent_no_dropout_test")
-  ) %>%
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios_2018-05-07_no_dropout_test_time_50_MC_samples/MNIST_accuracies_so_far_var_ratios.csv") %>% 
-      mutate(acq_fun = "var_rat_no_dropout_test")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/bald_2018-05-07_no_dropout_test_time_50_MC_samples/MNIST_accuracies_so_far_bald.csv") %>% 
-      mutate(acq_fun = "bald_no_dropout_test")
-  ) %>%
+
+bald_dirs = grep("bald", out_dirs, ignore.case = T, value = T)
+
+bald_accuracies = map_df(seq_along(bald_dirs), function(i){
+  file_name = grep("accuracies", list.files(bald_dirs[i], full.names = T), value = T)
+  out = read_csv(file_name) %>% 
+    mutate(run_id = i)
+})
+
+
+
+predictive_entropy_dirs = grep("predictive_entropy", out_dirs, ignore.case = T, value = T) %>% 
+  grep("freq", ., ignore.case = T, value = T, invert = T)
+
+predictive_entropy_accuracies = map_df(seq_along(predictive_entropy_dirs), function(i){
+  file_name = grep("accuracies", list.files(predictive_entropy_dirs[i], full.names = T), value = T)
+  out = read_csv(file_name) %>% 
+    mutate(run_id = i)
+})
+
+
+
+var_ratios_dirs = grep("var_ratios", out_dirs, ignore.case = T, value = T) %>% 
+  grep("freq", ., ignore.case = T, value = T, invert = T)
+
+var_ratios_accuracies = map_df(seq_along(var_ratios_dirs), function(i){
+  file_name = grep("accuracies", list.files(var_ratios_dirs[i], full.names = T), value = T)
+  out = read_csv(file_name) %>% 
+    mutate(run_id = i)
+})
+
+
+
+
+freq_predictive_entropy_dirs = grep("predictive_entropy", out_dirs, ignore.case = T, value = T) %>% 
+  grep("freq", ., ignore.case = T, value = T, invert = F)
+
+freq_predictive_entropy_accuracies = map_df(seq_along(freq_predictive_entropy_dirs), function(i){
+  file_name = grep("accuracies", list.files(freq_predictive_entropy_dirs[i], full.names = T), value = T)
+  out = read_csv(file_name) %>% 
+    mutate(run_id = i)
+})
+
+
+
+freq_var_ratios_dirs = grep("var_ratios", out_dirs, ignore.case = T, value = T) %>% 
+  grep("freq", ., ignore.case = T, value = T, invert = F)
+
+freq_var_ratios_accuracies = map_df(seq_along(freq_var_ratios_dirs), function(i){
+  file_name = grep("accuracies", list.files(freq_var_ratios_dirs[i], full.names = T), value = T)
+  out = read_csv(file_name) %>% 
+    mutate(run_id = i)
+})
+
+
+
+
+accuracies_all <- random_accuracies %>%
+  bind_rows(bald_accuracies) %>%
+  bind_rows(predictive_entropy_accuracies) %>%
+  bind_rows(var_ratios_accuracies) %>%
+  bind_rows(freq_predictive_entropy_accuracies) %>%
+  bind_rows(freq_var_ratios_accuracies) %>%
   mutate(num_images = 20 + (iter-1)*10)
-
-
-
-
-accuracies_all_2 <- read_csv("../out/MNIST/random_acq//MNIST_accuracies_so_far_random.csv") %>% 
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios/MNIST_accuracies_so_far_var_ratios.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/bald/MNIST_accuracies_so_far_bald.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/freq_var_ratios/MNIST_accuracies_so_far_freq_var_ratios.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/predictive_entropy/MNIST_accuracies_so_far_predictive_entropy.csv")
-  ) %>%
-  bind_rows(
-    read_csv("../out/MNIST/bald_2018-05-07_no_dropout_test_time/MNIST_accuracies_so_far_bald.csv") %>% 
-      mutate(acq_fun = "bald_no_dropout_test")
-  ) %>%
-  bind_rows(
-    read_csv("../out/MNIST/predictive_entropy_2018-05-07_no_dropout_test_time/MNIST_accuracies_so_far_predictive_entropy.csv") %>% 
-      mutate(acq_fun = "pred_ent_no_dropout_test")
-  ) %>%
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios_2018-05-07_no_dropout_test_time/MNIST_accuracies_so_far_var_ratios.csv") %>% 
-      mutate(acq_fun = "var_rat_no_dropout_test")
-  ) %>%
-  mutate(num_images = 20 + (iter-1)*10)
-
-
-
-read_csv("../out/MNIST/predictive_entropy/MNIST_accuracies_so_far_predictive_entropy.csv") %>%
-  bind_rows(
-    read_csv("../out/MNIST/freq_predictive_entropy/MNIST_accuracies_so_far_freq_predictive_entropy.csv")
-  ) %>% 
-  filter(accuracy > 0.6) %>%
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  geom_point(size = 0.3) +
-  geom_line(size = 0.2)
-
-
-
-
-read_csv("../out/MNIST/var_ratios/MNIST_accuracies_so_far_var_ratios.csv") %>%
-  bind_rows(
-    read_csv("../out/MNIST/freq_var_ratios/MNIST_accuracies_so_far_freq_var_ratios.csv")
-  ) %>% 
-  filter(accuracy > 0.6) %>%
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  geom_point(size = 0.3) +
-  geom_line(size = 0.2)
-
-
-
-read_csv("../out/MNIST/random_acq/MNIST_accuracies_so_far_random.csv") %>% 
-  bind_rows(
-    read_csv("../out/MNIST/var_ratios/MNIST_accuracies_so_far_var_ratios.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/bald/MNIST_accuracies_so_far_bald.csv")
-  ) %>% 
-  bind_rows(
-    read_csv("../out/MNIST/predictive_entropy/MNIST_accuracies_so_far_predictive_entropy.csv")
-  ) %>% 
-  filter(accuracy > 0.6) %>%
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  geom_point(size = 0.3) +
-  geom_line(size = 0.2)
-
-
-
-
-accuracies_all_2 %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-accuracies_all_2 %>% 
-  filter(acq_fun %in% c("bald", "bald_no_dropout_test",
-                        "predictive_entropy", "pred_ent_no_dropout_test",
-                        "var_ratios", "var_rat_no_dropout_test")) %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-
-accuracies_all_2 %>% 
-  filter(acq_fun %in% c("bald", "bald_no_dropout_test")) %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-accuracies_all_2 %>% 
-  filter(acq_fun %in% c("predictive_entropy", "pred_ent_no_dropout_test")) %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-accuracies_all_2 %>% 
-  filter(acq_fun %in% c("var_ratios", "var_rat_no_dropout_test")) %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-
-
-
 
 
 saveRDS(accuracies_all, "../out/MNIST/accuracies_all.rds")
-  
-accuracies_all %>% 
+
+
+accuracies_avg = accuracies_all %>% 
+  group_by(acq_fun, iter, num_images) %>% 
+  summarize(accuracy = mean(accuracy, na.rm = T))
+
+
+
+accuracies_avg %>% 
   ggplot(aes(num_images, accuracy, color = acq_fun)) +
-  geom_point() +
+  geom_point(size = 0.3) +
+  geom_line(size = 0.2)
+
+
+accuracies_avg %>% 
+  ggplot(aes(num_images, accuracy, color = acq_fun)) +
   geom_line()
 
 
-accuracies_all %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
-  #geom_point() +
-  geom_line()
-
-accuracies_all %>% 
-  filter(!is.na(accuracy)) %>% 
-  ggplot(aes(num_images, accuracy, color = acq_fun)) +
-  geom_line() 
-
-accuracies_all %>% 
+accuracies_avg %>% 
   filter(iter < 30) %>% 
-  ggplot(aes(iter, accuracy, color = acq_fun)) +
+  ggplot(aes(num_images, accuracy, color = acq_fun)) +
   geom_line() 
 
 
