6edcbf5c6f2982cbff2fec6e5839870248b68ab9
Mario <mbecerracontreras@gmail.com>
2018-12-09 22:56:21 -0600

    Use more images in experiment

--- code/CIFAR10_active_learning.R
+++ code/CIFAR10_active_learning.R
@@ -69,12 +69,17 @@ cat("Converted class vectors\n")
 #################################################################################
 #################################################################################
 
-seeds = c(1729, 4104, 13832)
+seeds = c(1729)
+# seeds = c(1729, 4104, 13832)
+n_epochs = 100
+n_images_per_iter = 1000
+n_acq_steps = 40
+nb_MC_samples = 100
 
 for(i in seq_along(seeds)){
   seed_i = seeds[i]
   
-  # Random initial set of 20 points for training, 100 for validation and the rest as pooling set
+  # Random initial set of 100 points for training, 100 for validation and the rest as pooling set
   initial_pool_train_val = create_initial_pool_train_val(y_all, seed_i)
   
   ix_train = initial_pool_train_val$ix_train
@@ -86,7 +91,7 @@ for(i in seq_along(seeds)){
   
   # Run funciton for random acquisition
   random_acquisition(
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -94,15 +99,15 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50,
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter,
     seed = seed_i)
   
   
   # Run funciton for FREQUENTIST var ratios
   frequentist_acquisition(
     acq_fun = 'freq_var_ratios', 
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -110,15 +115,15 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50)
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter)
   
   
   
   # Run funciton for FREQUENTIST predictive entropy
   frequentist_acquisition(
     acq_fun = 'freq_predictive_entropy', 
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -126,15 +131,15 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50)
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter)
   
   
   
   # Run funciton for variation ratios
   acquire_observations(
     acq_fun = 'var_ratios', 
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -142,9 +147,9 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50,
-    nb_MC_samples = 100
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter,
+    nb_MC_samples = nb_MC_samples
   )
   
   
@@ -152,7 +157,7 @@ for(i in seq_along(seeds)){
   # Run funciton for BALD
   acquire_observations(
     acq_fun = 'bald', 
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -160,9 +165,9 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50,
-    nb_MC_samples = 100)
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter,
+    nb_MC_samples = nb_MC_samples)
   
   
   
@@ -170,7 +175,7 @@ for(i in seq_along(seeds)){
   # Run funciton for predictive entropy
   acquire_observations(
     acq_fun = 'predictive_entropy', 
-    n_acq_steps = 50, 
+    n_acq_steps = n_acq_steps, 
     ix_train = initial_pool_train_val$ix_train, 
     ix_val = initial_pool_train_val$ix_val, 
     ix_pool = initial_pool_train_val$ix_pool, 
@@ -178,9 +183,9 @@ for(i in seq_along(seeds)){
     y_all = y_all, 
     x_test = x_test, 
     y_test = y_test, 
-    n_epochs = 200,
-    n_images_per_iter = 50,
-    nb_MC_samples = 100)
+    n_epochs = n_epochs,
+    n_images_per_iter = n_images_per_iter,
+    nb_MC_samples = nb_MC_samples)
   
   
 }
