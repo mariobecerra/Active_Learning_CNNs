bfad3201c8632206329a9554d2c5c16642e55d68
Mario <mbecerracontreras@gmail.com>
2018-12-09 21:38:54 -0600

    Save plots in PNG files in format ready to be put into latex

--- code/MNIST_plot_accuracies.R
+++ code/MNIST_plot_accuracies.R
@@ -25,7 +25,7 @@ palette_names %>%
 
 
 # Palette for acquisition functions
-cbPalette <- c("#0072B2", "#E69F00", "#56B4E9", "#009E73", "#000000","#CC79A7", "#D55E00", "#999999", "#F0E442")
+cbPalette <- c("#D55E00", "#E69F00", "#CC79A7", "#009E73", "#000000","#56B4E9", "#0072B2", "#999999", "#F0E442")
 
 names_colors = tibble(
   names = c("BALD", "Frequentist predictive entropy", "Frequentist variation ratios",
@@ -161,19 +161,20 @@ plot_out_dir = paste0(out_dir, "plots/")
 dir.create(plot_out_dir)
 
 # Plot file dimensions
-out_width = 20
-out_height = 10
+out_width = 13
+out_height = 11
 out_units = "cm"
 out_dpi = 300
 
 # Plot, axis and legend names
-plot_title = "MNIST dataset"
-x_label = "Number of images"
+plot_title = NULL #"MNIST dataset"
+x_label = NULL #"Number of images"
 y_label = "Accuracy on test set"
 legend_name = "Acquisition function"
 
 # Dataframe for plotting
 accuracies_avg = accuracies_all %>% 
+  mutate(accuracy = accuracy*100) %>% 
   group_by(acq_fun, iter, num_images) %>% 
   summarize(
     upper = quantile(accuracy, 0.95, na.rm = T),
@@ -181,24 +182,31 @@ accuracies_avg = accuracies_all %>%
     accuracy = mean(accuracy, na.rm = T)
   )
 
-## All acquisition functions
+
+## Bayesian acquisition functions
 
 (
   accuracies_avg %>% 
+    filter(acq_fun %in% c("var_ratios", "predictive_entropy", "bald", "random")) %>% 
     left_join(names_colors) %>% 
     ggplot(aes(num_images, accuracy, color = names)) +
     geom_point(size = 0.3) +
     geom_line(size = 0.4) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+    scale_y_continuous(breaks = seq(60, 100, by = 2)) +
+    scale_x_continuous(breaks = seq(0, 1000, by = 100)) +
+    coord_cartesian(ylim = c(80, 100),
+                    xlim = c(100, 1000))  +
     my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1),
+          panel.border = element_blank()) +
+    labs(
+      title = plot_title,
+      x = x_label, 
+      y = y_label, 
+      color = legend_name) +
     scale_color_manual(values = names_colors_vec)
 ) %>% 
-  ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_all.png"), 
+  ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_bayesian.png"), 
          .,
          width = out_width,
          height = out_height,
@@ -206,57 +214,82 @@ accuracies_avg = accuracies_all %>%
          dpi = out_dpi)
 
 
-
-(
-  accuracies_avg %>% 
-    left_join(names_colors) %>% 
-    ggplot(aes(num_images, accuracy)) +
-    geom_point(size = 0.3, aes(color = names)) +
-    geom_line(size = 0.4, aes(color = names)) +
-    geom_ribbon(aes(ymin = lower, ymax = upper, fill = names), alpha = 0.3, color = NA) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
-    my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name,
-         fill = legend_name) +
-    scale_color_manual(values = names_colors_vec) +
-    scale_fill_manual(values = names_colors_vec)
-) %>% 
-  ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_all_ribbon.png"), 
-         .,
-         width = out_width,
-         height = out_height,
-         units = out_units,
-         dpi = out_dpi)
+# ## All acquisition functions
+# 
+# (
+#   accuracies_avg %>% 
+#     left_join(names_colors) %>% 
+#     ggplot(aes(num_images, accuracy)) +
+#     geom_point(size = 0.3, aes(color = names)) +
+#     geom_line(size = 0.4, aes(color = names)) +
+#     scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
+#     scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+#     my_theme() +
+#     labs(title = plot_title, 
+#          x = x_label, 
+#          y = y_label, 
+#          color = legend_name,
+#          fill = legend_name) +
+#     scale_color_manual(values = names_colors_vec) 
+# ) %>% 
+#   ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_all.png"), 
+#          .,
+#          width = out_width,
+#          height = out_height,
+#          units = out_units,
+#          dpi = out_dpi)
+# 
+# 
+# (
+#   accuracies_avg %>% 
+#     left_join(names_colors) %>% 
+#     ggplot(aes(num_images, accuracy)) +
+#     geom_point(size = 0.3, aes(color = names)) +
+#     geom_line(size = 0.4, aes(color = names)) +
+#     geom_ribbon(aes(ymin = lower, ymax = upper, fill = names), alpha = 0.3, color = NA) +
+#     scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
+#     scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+#     my_theme() +
+#     labs(title = plot_title, 
+#          x = x_label, 
+#          y = y_label, 
+#          color = legend_name,
+#          fill = legend_name) +
+#     scale_color_manual(values = names_colors_vec) +
+#     scale_fill_manual(values = names_colors_vec)
+# ) %>% 
+#   ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_all_ribbon.png"), 
+#          .,
+#          width = out_width,
+#          height = out_height,
+#          units = out_units,
+#          dpi = out_dpi)
 
 
 ## Predictive entropy
 
-(
-  accuracies_avg %>% 
-    left_join(names_colors) %>% 
-    filter(acq_fun %in% c("freq_predictive_entropy", "predictive_entropy")) %>% 
-    ggplot(aes(num_images, accuracy, color = names)) +
-    geom_point(size = 0.3) +
-    geom_line(size = 0.4) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
-    my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name) +
-    scale_color_manual(values = names_colors_vec)
-) %>% 
-  ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_pred_ent.png"), 
-         .,
-         width = out_width,
-         height = out_height,
-         units = out_units,
-         dpi = out_dpi)
+# (
+#   accuracies_avg %>% 
+#     left_join(names_colors) %>% 
+#     filter(acq_fun %in% c("freq_predictive_entropy", "predictive_entropy")) %>% 
+#     ggplot(aes(num_images, accuracy, color = names)) +
+#     geom_point(size = 0.3) +
+#     geom_line(size = 0.4) +
+#     scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
+#     scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+#     my_theme() +
+#     labs(title = plot_title, 
+#          x = x_label, 
+#          y = y_label, 
+#          color = legend_name) +
+#     scale_color_manual(values = names_colors_vec)
+# ) %>% 
+#   ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_pred_ent.png"), 
+#          .,
+#          width = out_width,
+#          height = out_height,
+#          units = out_units,
+#          dpi = out_dpi)
 
 
 
@@ -265,17 +298,22 @@ accuracies_avg = accuracies_all %>%
     left_join(names_colors) %>% 
     filter(acq_fun %in% c("freq_predictive_entropy", "predictive_entropy")) %>% 
     ggplot(aes(num_images, accuracy)) +
-    geom_point(size = 0.3, aes(color = names)) +
-    geom_line(size = 0.4, aes(color = names)) +
+    geom_point(size = 0.6, aes(color = names)) +
+    geom_line(size = 0.5, aes(color = names)) +
     geom_ribbon(aes(ymin = lower, ymax = upper, fill = names), alpha = 0.3, color = NA) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+    scale_y_continuous(breaks = seq(60, 100, by = 2)) +
+    scale_x_continuous(breaks = seq(0, 1000, by = 100)) +
+    coord_cartesian(ylim = c(80, 100),
+                    xlim = c(100, 1000))  +
     my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name,
-         fill = legend_name) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1),
+          panel.border = element_blank()) +
+    labs(
+      title = plot_title,
+      x = x_label, 
+      y = y_label, 
+      color = legend_name,
+      fill = legend_name) +
     scale_color_manual(values = names_colors_vec) +
     scale_fill_manual(values = names_colors_vec)
 ) %>% 
@@ -289,30 +327,30 @@ accuracies_avg = accuracies_all %>%
 
 
 ## Variation ratios
-
-(
-  accuracies_avg %>% 
-    left_join(names_colors) %>% 
-    filter(acq_fun %in% c("freq_var_ratios", "var_ratios")) %>% 
-    ggplot(aes(num_images, accuracy, color = names)) +
-    geom_point(size = 0.3) +
-    geom_line(size = 0.4) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
-    my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name) +
-    scale_color_manual(values = names_colors_vec)
-) %>% 
-  ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_var_ratios.png"), 
-         .,
-         width = out_width,
-         height = out_height,
-         units = out_units,
-         dpi = out_dpi)
-
+# 
+# (
+#   accuracies_avg %>% 
+#     left_join(names_colors) %>% 
+#     filter(acq_fun %in% c("freq_var_ratios", "var_ratios")) %>% 
+#     ggplot(aes(num_images, accuracy, color = names)) +
+#     geom_point(size = 0.3) +
+#     geom_line(size = 0.4) +
+#     scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
+#     scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+#     my_theme() +
+#     labs(title = plot_title, 
+#          x = x_label, 
+#          y = y_label, 
+#          color = legend_name) +
+#     scale_color_manual(values = names_colors_vec)
+# ) %>% 
+#   ggsave(filename = paste0(plot_out_dir, "MNIST_accuracies_var_ratios.png"), 
+#          .,
+#          width = out_width,
+#          height = out_height,
+#          units = out_units,
+#          dpi = out_dpi)
+# 
 
 
 (
@@ -320,17 +358,22 @@ accuracies_avg = accuracies_all %>%
     left_join(names_colors) %>% 
     filter(acq_fun %in% c("freq_var_ratios", "var_ratios")) %>% 
     ggplot(aes(num_images, accuracy)) +
-    geom_point(size = 0.3, aes(color = names)) +
-    geom_line(size = 0.4, aes(color = names)) +
+    geom_point(size = 0.6, aes(color = names)) +
+    geom_line(size = 0.5, aes(color = names)) +
     geom_ribbon(aes(ymin = lower, ymax = upper, fill = names), alpha = 0.3, color = NA) +
-    scale_y_continuous(breaks = seq(0.6, 1, by = 0.05)) +
-    scale_x_continuous(breaks = seq(0, 1200, by = 20)) +
+    scale_y_continuous(breaks = seq(60, 100, by = 2)) +
+    scale_x_continuous(breaks = seq(0, 1000, by = 100)) +
+    coord_cartesian(ylim = c(80, 100),
+                    xlim = c(100, 1000))  +
     my_theme() +
-    labs(title = plot_title, 
-         x = x_label, 
-         y = y_label, 
-         color = legend_name,
-         fill = legend_name) +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1),
+          panel.border = element_blank()) +
+    labs(
+      title = plot_title,
+      x = x_label, 
+      y = y_label, 
+      color = legend_name,
+      fill = legend_name) +
     scale_color_manual(values = names_colors_vec) +
     scale_fill_manual(values = names_colors_vec)
 ) %>% 
